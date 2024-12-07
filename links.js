66649d8395b8d573e111b321ff464376abb307ae
DavideB45 <dadewww@gmail.com>
2024-12-07 12:42:04 +0100

    first test

---
+++ links.js
@@ -0,0 +1,6 @@
+const links = [
+    { name: "Group 1 Documentation", url: "https://github.com/group1/documentation" },
+    { name: "Group 2 Documentation", url: "https://github.com/group2/documentation" },
+    { name: "Group 3 Documentation", url: "https://github.com/group3/documentation" },
+    // Add more links as needed
+];
