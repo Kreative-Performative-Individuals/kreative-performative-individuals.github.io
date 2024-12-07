34f3824e8b15e7a3bc1297ad982dd11fd934d304
DavideB45 <dadewww@gmail.com>
2024-12-07 12:59:17 +0100

    Done

--- links.js
+++ links.js
@@ -1,6 +1,10 @@
 const links = [
-    { name: "Example", url: "https://it.wikipedia.org/wiki/Pagina_principale" },
-    { name: "Group 2 Documentation", url: "https://github.com/group2/documentation" },
-    { name: "Group 3 Documentation", url: "https://github.com/group3/documentation" },
+    { name: "Group 1 Knowledge Base", url: "https://kreative-performative-individuals.github.io/KB/" },
+    { name: "Group 2 Database", url: "https://kreative-performative-individuals.github.io/smart-industrial-database/" },
+    { name: "Group 3 Data Processing", url: "https://github.com/group3/documentation" },
+    { name: "Group 4 API", url: "https://kreative-performative-individuals.github.io/api-gateway/" },
+    { name: "Group 5 RAG", url: "https://kreative-performative-individuals.github.io/rag/" },
+    { name: "Group 6 UI", url: "https://kreative-performative-individuals.github.io/ui/" },
+    { name: "Group 8 KPI Calculation Engine", url: "https://kreative-performative-individuals.github.io/kpi-calculation-engine/" },
     // Add more links as needed
 ];
