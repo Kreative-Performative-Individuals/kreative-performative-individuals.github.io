1030b0e9a341bd3f1b51c4d257640a07954d565c
DavideB45 <dadewww@gmail.com>
2024-12-07 12:44:29 +0100

    test 2

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const links = [
-    { name: "Group 1 Documentation", url: "https://github.com/group1/documentation" },
+    { name: "Example", url: "https://it.wikipedia.org/wiki/Pagina_principale" },
     { name: "Group 2 Documentation", url: "https://github.com/group2/documentation" },
     { name: "Group 3 Documentation", url: "https://github.com/group3/documentation" },
     // Add more links as needed
