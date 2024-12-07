76d4bb596064ced528edc45bcdd777e889e3468f
Andrea Marino <134485532+M4rinz@users.noreply.github.com>
2024-12-07 17:56:26 +0100

    Added link to Group 3 documentation, added logo, minor tweaks

--- links.js
+++ links.js
@@ -1,10 +1,10 @@
 const links = [
-    { name: "Group 1 Knowledge Base", url: "https://kreative-performative-individuals.github.io/KB/" },
-    { name: "Group 2 Database", url: "https://kreative-performative-individuals.github.io/smart-industrial-database/" },
-    { name: "Group 3 Data Processing", url: "https://github.com/group3/documentation" },
-    { name: "Group 4 API", url: "https://kreative-performative-individuals.github.io/api-gateway/" },
-    { name: "Group 5 RAG", url: "https://kreative-performative-individuals.github.io/rag/" },
-    { name: "Group 6 UI", url: "https://kreative-performative-individuals.github.io/ui/" },
-    { name: "Group 8 KPI Calculation Engine", url: "https://kreative-performative-individuals.github.io/kpi-calculation-engine/" },
+    { name: "Group 1 - Knowledge Base", url: "https://kreative-performative-individuals.github.io/KB/" },
+    { name: "Group 2 - Database", url: "https://kreative-performative-individuals.github.io/smart-industrial-database/" },
+    { name: "Group 3 - Data Processing", url: "https://kreative-performative-individuals.github.io/data-preprocessing-/" },
+    { name: "Group 4 - API", url: "https://kreative-performative-individuals.github.io/api-gateway/" },
+    { name: "Group 5 - RAG", url: "https://kreative-performative-individuals.github.io/rag/" },
+    { name: "Group 6 - UI", url: "https://kreative-performative-individuals.github.io/ui/" },
+    { name: "Group 8 - KPI Calculation Engine", url: "https://kreative-performative-individuals.github.io/kpi-calculation-engine/" },
     // Add more links as needed
 ];
